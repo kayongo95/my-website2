43dd98d0d34af75f453a14d7e6920aaffb0ae6a4
Kayongo Nicholas <nicklasoncrystal@gmail.com>
2024-08-23 18:56:44 +0300

    Update website2.js

--- website2.js
+++ website2.js
@@ -1,18 +1,4 @@
-// let myMenu = document.getElementById('menu');
-// let myIcon = document.getElementById('icon');
 
-
-// function toggleMenu(){
-//     (myMenu.matches (max-width, '768px'));
-//     if(myMenu.style.display === 'none'){
-//         myMenu.style.display = 'flex';
-//         icon.style.transform = 'rotate(180deg)';
-//     }
-//     else{
-//         myMenu.style.display = 'none';
-//         icon.style.transform = 'rotate(0deg)';
-//     }
-// }
 
 function toggleMenu() {
     const mediaQuery = window.matchMedia("(max-width: 768px)");
